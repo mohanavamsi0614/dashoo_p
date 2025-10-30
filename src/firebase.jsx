b048e6d5a8d1b2cd147d7bd54e6789a9a7c46e52
Mohana Vamsi <mohanavamsi14@gmail.com>
2025-10-30 18:07:08 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -209,10 +209,10 @@ function GoogleAuth({text}) {
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
                 </svg>
-                Submitting...
+                Createing...
               </>
             ) : (
-              'Submit Registration'
+              'Create Account'
             )}
           </button>
         </div>
