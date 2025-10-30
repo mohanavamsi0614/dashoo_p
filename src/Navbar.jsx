500a1c7ea49f16723937b3c18c6c60491ff1d648
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-10-30 09:15:26 +0530

    done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router";
-import GoogleAuth from "./firebase";
 
 function Navbar() {
+  const nav=useNavigate()
   return (
     <div className="text-2xl sm:text-3xl md:text-4xl mx-4 sm:mx-6 md:mx-10 font-poppins text-white p-3 sm:p-4 md:p-5 flex justify-between items-center rounded-full">
       <div>
