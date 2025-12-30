65b37e71b8e06a678451569291b7c82074e6c9d1
mohanavamsi0614 <mohanavamsi14@gmail.com>
2025-12-30 13:33:55 +0530

    doone

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import './index.css'
 import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from "@vercel/analytics/next"
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Analytics></Analytics>
   </BrowserRouter>
 )
